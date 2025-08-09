95e30ef542c1edbac2830b346c5aa78be8e6a0d5
Sirsuave50 <ladams1221@gmail.com>
2025-08-09 16:28:25 -0700

    V3

---
+++ sw.js
@@ -0,0 +1,21 @@
+// Offline-first service worker for Mini QWOP v3
+const CACHE = 'qwop-v3';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k===CACHE?null:caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url);
+  if (url.origin === location.origin) {
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  }
+});
